1830b412b1240515b6e5b0e4459e5908a3b46d57
HimaShah146 <hima.shah@aliansoftware.com>
2025-06-06 12:08:00 +0530

    commit

--- Sesstion_1/Selenium_Locators.py
+++ Sesstion_1/Selenium_Locators.py
@@ -4,14 +4,25 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://money.rediff.com/gainers/bse/daily/groupall")
-msg = driver.find_element(By.XPATH,"//a[text()='GCM Capital Advisors']/self::a").text
-msg1 = driver.find_element(By.XPATH,"(//tr)[10]/descendant::td[3]").text
-msg2 = driver.find_element(By.XPATH,"//a[normalize-space()='GCM Capital Advisors']/ancestor:: tr").text
-msg3 = driver.find_element(By.XPATH,"//a[normalize-space()='GCM Capital Advisors']/ancestor:: tr/child::td[5]").text
-msg4 = driver.find_element(By.XPATH,"//a[normalize-space()='GCM Capital Advisors']/ancestor:: tr/preceding-sibling::tr[1]").text
-msg5 = driver.find_elements(By.XPATH,"//a[normalize-space()='GCM Capital Advisors']/ancestor:: tr/following-sibling::tr")
-msg6 = driver.find_element(By.XPATH,"//a[normalize-space()='GCM Capital Advisors']/ancestor:: tr/following-sibling::tr").text
-print("Self Locator: ",msg,"\nDescendant: ",msg1,"\nAncestor: ",msg2,"\nChild: ",msg3,"\nPreceding Sibling: ",msg4,"\nFollowing Sibling: ",msg6)
-print(len(msg5))
+msg = driver.find_element(By.XPATH,"//a[text()='GCM Capital Advisors']/self::a").text # GCM Capital Advisors
 
+msg1 = driver.find_element(By.XPATH,"(//tr)[10]/descendant::td[3]").text #6.97
+
+msg2 = driver.find_element(By.XPATH,"//a[text()='GCM Capital Advisors']/ancestor:: tr").text #GCM Capital Advisors M 6.97 8.00 + 14.78 Buy  |  Sell
+
+msg3 = driver.find_element(By.XPATH,"//a[text()='GCM Capital Advisors']/ancestor:: tr/child::td[5]").text #+ 14.78
+
+msg4 = driver.find_element(By.XPATH,"//a[text()='GCM Capital Advisors']/ancestor:: tr/preceding-sibling::tr[1]").text #Ajcon Global Service X 72.21 83.75 + 15.98 Buy  |  Sell
+
+msg5 = driver.find_elements(By.XPATH,"//a[text()='GCM Capital Advisors']/ancestor:: tr/following-sibling::tr") # 2021
+
+msg6 = driver.find_element(By.XPATH,"//a[text()='GCM Capital Advisors']/ancestor:: tr/following-sibling::tr").text #  Hubtown B 181.20 206.90 + 14.18 Buy  |  Sell
+
+msg7 = driver.find_element(By.XPATH,"//a[text()='GCM Capital Advisors']/parent::td").text
+
+msg8 = driver.find_elements(By.XPATH,"//a[text()='GCM Capital Advisors']/following::tr")
+
+print("Self Locator: ",msg,"\nDescendant: ",msg1,"\nAncestor: ",msg2,"\nChild: ",msg3,"\nPreceding Sibling: ",msg4,"\nFollowing Sibling: ",msg6,"\nParent: ",msg7 )
+print(len(msg5))
+print(len(msg8))
 
