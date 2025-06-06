66b25ec1bc0817766a2c9c023bfc753318c247e2
HimaShah146 <hima.shah@aliansoftware.com>
2025-06-06 17:51:20 +0530

    commit

--- Sesstion_1/Selenium_Locators.py
+++ Sesstion_1/Selenium_Locators.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -25,4 +26,7 @@ msg8 = driver.find_elements(By.XPATH,"//a[text()='GCM Capital Advisors']/followi
 print("Self Locator: ",msg,"\nDescendant: ",msg1,"\nAncestor: ",msg2,"\nChild: ",msg3,"\nPreceding Sibling: ",msg4,"\nFollowing Sibling: ",msg6,"\nParent: ",msg7 )
 print(len(msg5))
 print(len(msg8))
-
+time.sleep(3)
+driver.get("https://money.rediff.com/gainers/bse/daily/groupz")
+v= driver.find_element(By.XPATH,"//a[text()='HDIL']/following::tr[2]").text
+print(v)
